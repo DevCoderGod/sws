1ab1136e5dbe942cea8c7fab51b3108c224b0580
DevCoderGod <devcodergod@gmail.com>
2023-12-26 20:32:26 +0300

    Header to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import S from './App.module.scss';
-import cn from "classnames"
-import {Icon} from './icons/Icon'
+import { Header } from './components/Header'
 
 function App() {
   return (
     <div className={S.App}>
-		<div className={S.header}>
-			<div className={S.icon}><Icon name='menu'/></div>
-			<div className={S.icon}><Icon name='back'/></div>
-			<div className={S.menu}>
-				<div className={cn(S.menu__point, S.menu__point_active)}>Просмотр</div>
-				<div className={S.menu__point}>Управление</div>
-			</div>
-			
-		</div>
+		<Header/>
     </div>
   );
 }
