b01f2f70a470d6ee901a6278d05de0b103860e8e
DevCoderGod <devcodergod@gmail.com>
2024-01-04 22:30:24 +0300

    text...

--- src/components/DataRows/Row/Row.service.ts
+++ src/components/DataRows/Row/Row.service.ts
@@ -18,8 +18,11 @@ export function GetCellProps(currentRow:IRow, ref: React.MutableRefObject<IRow>)
 				ref.current = {...ref.current, [fieldName]: value}
 			},
 			stopEditing: async() => {
-				if(currentRow.id === 0){ setRowsAction(recalculatedRow(rows, await createRow(ref.current).unwrap(), ref.current)) }
-				if(currentRow.id > 0){ setRowsAction(recalculatedRow(rows, await updateRow(ref.current).unwrap(), ref.current)) }
+				if(currentRow.id === 0)
+					setRowsAction(recalculatedRow(rows, await createRow(ref.current).unwrap(), ref.current))
+				if(currentRow.id > 0)
+					setRowsAction(recalculatedRow(rows, await updateRow(ref.current).unwrap(), ref.current))
+
 				stopEditingAction()
 			}
 		}
@@ -29,6 +32,59 @@ export function GetCellProps(currentRow:IRow, ref: React.MutableRefObject<IRow>)
 			startEditing: () => rowEditable === null && startEditingAction(currentRow.id)
 		}
 }
+
+export function GetLevelProps(row:IRow, index:number):ILevelProps{
+	const {rows} = useAppState(state => state.Rows)
+	const {setRowsAction, addRowAction, startEditingAction} = useActions()
+	const [deleteRow] = Api.useDeleteRowMutation()
+
+	return({
+		index,
+		id:row.id,
+		level:row.level,
+		pID:row.parentId,
+		createNewRow:()=>{
+			addRowAction({
+				index: getIndexForNewLine(rows, index),
+				newRow: createEmptyRow(row.level+1, row.id)
+			})
+			startEditingAction(0)
+		},
+		deleteRow: async() => {
+			const response = await deleteRow(row.id).unwrap()
+			const newRows = [...rows]
+
+			if(row.parentId){
+				const parentIndex = newRows.findIndex(orow => orow.id ===row.parentId)
+				const parentRow:({index:number, row:IRow}) = {
+					index: parentIndex,
+					row: {...newRows[parentIndex], total:newRows[parentIndex].total-1}
+				}
+				newRows.splice(parentRow.index,1,parentRow.row)
+		
+			}
+			response.changed.forEach(changedRow=>{
+				const oldRow = newRows.find(row => row.id ===changedRow.id)
+				if(oldRow){
+					const total = oldRow.total
+					const parentId = oldRow.parentId
+					const level = oldRow.level
+					const index = newRows.findIndex(row => row.id === oldRow.id)
+					const newRow :IRow = {...changedRow, parentId, level, total}
+					newRows.splice(index,1,newRow)
+				}
+			})
+			newRows.splice(index,1)
+
+			clearRows(newRows)
+
+			if(newRows.length === 0) newRows.push(...getRows([]))
+			setRowsAction(newRows)
+		}
+	})
+}
+
+
 
 function recalculatedRow(rows: IRow[], response:IRowResponse, currentRow:IRow): IRow[] {
 	const newRows = [...rows]
@@ -68,60 +124,6 @@ export function getIndexForNewLine(rows: IRow[], index:number):number{
 	if(cIndex < 0) return index+1
 	if(rows[cIndex].total === 0) return cIndex+1
 	return getIndexForNewLine(rows,cIndex)
-}
-
-export function GetLevelProps(row:IRow, index:number):ILevelProps{
-	const {rows} = useAppState(state => state.Rows)
-	const {setRowsAction} = useActions()
-	const [deleteRow] = Api.useDeleteRowMutation()
-	const {addRowAction, startEditingAction} = useActions()
-
-	const i = getIndexForNewLine(rows, index)
-
-	return({
-		index,
-		id:row.id,
-		level:row.level,
-		pID:row.parentId,
-		createNewRow:()=>{
-			addRowAction({
-				index: i,
-				newRow: createEmptyRow(row.level+1, row.id)
-			})
-			startEditingAction(0)
-		},
-		deleteRow: async() => {
-			const response = await deleteRow(row.id).unwrap()
-			const newRows = [...rows]
-
-			if(row.parentId){
-				const parentIndex = newRows.findIndex(orow => orow.id ===row.parentId)
-				const parentRow:({index:number, row:IRow}) = {
-					index: parentIndex,
-					row: {...newRows[parentIndex], total:newRows[parentIndex].total-1}
-				}
-				newRows.splice(parentRow.index,1,parentRow.row)
-		
-			}
-			response.changed.forEach(changedRow=>{
-				const oldRow = newRows.find(row => row.id ===changedRow.id)
-				if(oldRow){
-					const total = oldRow.total
-					const parentId = oldRow.parentId
-					const level = oldRow.level
-					const index = newRows.findIndex(row => row.id === oldRow.id)
-					const newRow :IRow = {...changedRow, parentId, level, total}
-					newRows.splice(index,1,newRow)
-				}
-			})
-			newRows.splice(index,1)
-
-			clearRows(newRows)
-
-			if(newRows.length === 0) newRows.push(...getRows([]))
-			setRowsAction(newRows)
-		}
-	})
 }
 
 function clearRows(rows:IRow[]) {
